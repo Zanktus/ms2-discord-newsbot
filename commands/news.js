84b96523ccde76c9726f9b40b6aeba5823325939
Denny <zanktus@gmail.com>
2018-09-18 14:10:19 +0200

    Added first crawl draft

--- commands/news.js
+++ commands/news.js
@@ -1,1 +1,29 @@
-// coming soon
+const Crawler = require("simplecrawler"),
+      cheerio = require("cheerio"),
+      Discord = module.require("discord.js"),
+      fs = module.require("fs"),
+      source = "http://maplestory2.nexon.net/en/news/";
+
+exports.run = async (client) => {
+  let crawler = new Crawler(source);
+  
+  crawler.interval = 30000;
+  crawler.maxConcurrency = 3;
+  crawler.maxDepth = 1;
+  crawler.decodeResponses = true;
+  
+  crawler.on("crawlstart", () => {
+    let crawlTime = new Date().toTimeString().replace(/.*(\d{2}:\d{2}:\d{2}).*/, "$1");
+    console.log(`[news](${crawlTime}): MS2 Crawler started`);
+  });
+  
+  crawler.on("fetchcomplete", (queueItem, responseBuffer, response) => {
+    // coming soon
+  });
+  
+  crawler.start();
+}
+
+exports.help = {
+  name: "news"
+}
